a92f38588c5865b3423c0aede3c0a07727cf5a42
sahoo <sahoosibaprasad2221@gmail.com>
2023-12-04 01:12:30 +0530

    ans

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -8,12 +8,20 @@ public class Main {
 }
 
 class A{
+
+    A(){
+
+    }
     public String meth(){
         return "Invoking method from class A";
     }
 }
 
 class B extends A{
+
+    B(){
+
+    }
     @Override
     public String meth(){
         return "Method is overridden in Extendend class B";
